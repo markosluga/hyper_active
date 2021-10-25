f327e5ef864d9d29e5e904d1ccdbd538d6df6f32
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-24 21:54:01 -0400

    Update enable-wrm.ps1

--- enable-wrm.ps1
+++ enable-wrm.ps1
@@ -1,2 +1,5 @@
 Enable-NetFirewallRule -DisplayName "Windows Remote Management - Compatibility Mode (HTTP-In)"
 Enable-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
+Enable-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
+Enable-PSRemoting
+Set-Service -Name "WinRM" -Status running -StartupType automatic
